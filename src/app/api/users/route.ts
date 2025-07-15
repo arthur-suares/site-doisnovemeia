bcf04803f3701d0e41f8535f455f44885d288999
arthur-suares <arthur.lantr@gmail.com>
2025-07-15 16:14:45 -0300

    [feat] Adicionando criptografia para senhas de usuário

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,7 @@
 // app/api/users/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import { getAllUsers, createUser } from '../../../controllers/userController'; // use @ se estiver com path aliases
+import { getAllUsers, createUser } from '../../../controllers/userController'; 
+import bcrypt from 'bcrypt'
 
 export async function GET() {
   try {
@@ -15,16 +16,23 @@ export async function GET() {
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const { name, email } = body;
+    const { name, email, password } = body;
 
-    if (!name || !email) {
+    if (!name || !email || !password) {
       return NextResponse.json({ error: 'Nome e email são obrigatórios' }, { status: 400 });
     }
 
+    // Encriptar a senha antes de armazenar
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    
     // ✅ Não passe `id` aqui
-    const newUser = await createUser(name, email);
+    const newUser = await createUser(name, email, hashedPassword);
 
-    return NextResponse.json(newUser, { status: 201 });
+    // Usando desestruturação com renomeação para não retornar a senha da resposta da requisição
+    const { password: _, ...userWithoutPassword } = newUser;
+
+    return NextResponse.json(userWithoutPassword, { status: 201 });
   } catch (error) {
     console.error('[POST USER ERROR]', error);
     return NextResponse.json({ error: 'Erro ao criar usuário' }, { status: 500 });
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,13 +2,20 @@
 import { prisma } from '../models/prisma';
 
 export async function getAllUsers() {
-  return await prisma.user.findMany();
+  return await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      //não deixar password ser puxado
+    }
+  });
 }
 
-export async function createUser(name: string, email: string) {
+export async function createUser(name: string, email: string, password: string) {
   try {
     return await prisma.user.create({
-      data: { name, email },
+      data: { name, email, password},
     });
   } catch (error) {
     console.error('---- Detailed Prisma error:', error);
